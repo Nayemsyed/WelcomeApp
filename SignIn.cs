030ca84d16de0051b7557376e8edad6d1098dcfe
Nayemsyed <sdnayem04112000@gmail.com>
2024-02-15 14:44:05 +0530

    Final Commit

---
+++ SignIn.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WelcomeApp
+{
+    public class SignIn
+    {
+        public string Authenticate(string userName, string password)
+        {
+            string msg;
+            if ((string.IsNullOrEmpty(userName)) || string.IsNullOrEmpty(password))
+            {
+                msg = "user name and passwordd is required";
+
+            }
+            else
+            {
+                if ((userName == "admin") && (password == "admin@123"))
+                {
+                    msg = "Authentication Pass";
+                }
+                else
+                {
+
+                    msg = "Authentication Fail";
+                }
+            }
+        
+        return msg;
+            }
+        }
+    }
+
